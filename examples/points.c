03255c6138f47951f92ecc895896ca52f1d57e85
Ankur Seth <iankurseth@gmail.com>
2020-11-07 04:28:26 +0530

    Added first example: points.c with code copied from main.c

---
+++ examples/points.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//#define VEC_IMPORT_FULL
+#include "vec.h"
+
+struct Point {
+	int x, y, z;
+};
+
+int
+main (int argc, char const *argv[])
+{
+	Vec v = vec->new();
+	for (int i = 0; i < 50; ++i) {
+		struct Point *p = malloc(sizeof(struct Point));
+		p->x = i;
+		p->y = i * 2;
+		p->z = i * 3;
+		vec->push(v, p);
+	}
+
+	foreach_vec (index, v) {
+		struct Point *ps = v->data[index];
+		printf("Point (%d, %d, %d)\n", ps->x, ps->y, ps->z);
+	}
+	v = vec->delete(v);
+	return 0;
+}
