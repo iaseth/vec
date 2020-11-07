b01952a034cc4b2f83db2b54bf8f5b3cb51d03bc
Ankur Seth <iankurseth@gmail.com>
2020-11-07 05:43:37 +0530

    Cleaned main.c.

--- src/main.c
+++ src/main.c
@@ -4,26 +4,9 @@
 //#define VEC_IMPORT_FULL
 #include "vec.h"
 
-struct Point {
-	int x, y, z;
-};
-
 int
 main (int argc, char const *argv[])
 {
-	Vec v = vec->new();
-	for (int i = 0; i < 50; ++i) {
-		struct Point *p = malloc(sizeof(struct Point));
-		p->x = i;
-		p->y = i * 2;
-		p->z = i * 3;
-		vec->push(v, p);
-	}
-
-	foreach_vec (index, v) {
-		struct Point *ps = v->data[index];
-		printf("Point (%d, %d, %d)\n", ps->x, ps->y, ps->z);
-	}
-	v = vec->delete(v);
+	printf("Hello, Vec!\n");
 	return 0;
 }
